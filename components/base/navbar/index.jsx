6980ed5de6cb613cb2d9ab2214d4b4393993d748
Adrian Ardizza <adrian.ardizza@gmail.com>
2021-04-20 23:03:33 +0700

    Adjusted menu spacing

--- components/base/navbar/index.jsx
+++ components/base/navbar/index.jsx
@@ -2,16 +2,16 @@ import Link from 'next/link';
 import Item from './MenuItem';
 
 const Navbar = () => (
-  <div className="fixed w-full py-3">
-    <div className="flex justify-between items-center max-w-5xl mx-auto text-white">
+  <div className="fixed w-full py-4 px-6">
+    <div className="flex justify-between items-center mx-auto text-white">
       <Link href="/">
-        <h1 className="font-semibold cursor-pointer select-none">
+        <h1 className="text-xl font-semibold cursor-pointer select-none">
           adrian.ardizza
         </h1>
       </Link>
-      <div className="flex text-xs font-light">
+      <div className="flex font-light text-md">
         <Item href="/">
-          <a className="mr-3">home</a>
+          <a className="mr-5">home</a>
         </Item>
         <Item href="/about">
           <a>about</a>
